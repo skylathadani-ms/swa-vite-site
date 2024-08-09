6563876bfb8561915caab4f0df69cf2203cec0b2
Timothy Wang <30346660+Timothyw0@users.noreply.github.com>
2024-08-09 10:40:15 -0400

    change (#1)
    Co-authored-by: Timothy Wang <timothywang@microsoft.com>

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <p>
-        Hello new change. Hello world! Howdy 🤠! CRUD after I got unblocked! Make a change again for environment!!
+        Hello new change. Hello world! Howdy 🤠! CRUD after I got unblocked! Make a change on new branch!!
       </p>
     </>
   )
