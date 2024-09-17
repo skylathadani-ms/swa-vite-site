1348ede9bb703d4477bc9111df2cf3e23212b2d2
Timothy Wang <timothywang@microsoft.com>
2024-09-17 17:21:58 -0400

    change api

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -2,6 +2,6 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.res = {
-        body: ["Jarod", "Hannah", "Kevin", "Mitch","Colby", "Hello world", "New API Change by Tim", "Hello world v2", "Api change back to working"]
+        body: ["Jarod", "Hannah", "Kevin", "Mitch","Colby", "Hello world", "New API Change by Tim", "Hello world v2", "Api change now it should fail"]
     }
 };
